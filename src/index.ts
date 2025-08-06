2e06990dd7f28f014e039989ba39f44cdaae732f
rizypb <rizwanul.rudra@ypbsystems.com>
2025-08-06 18:00:20 +0700

    fix: async component fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { default as QRCodeVue3 } from "./QRCodeVue3.vue";
-import GS1QRCodeVue3 from "./GS1QRCodeVue3.vue";
+import { default as GS1QRCodeVue3 } from "./GS1QRCodeVue3.vue";
 
 export { QRCodeVue3, GS1QRCodeVue3 };
