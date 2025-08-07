d7d4d695815fafe7d0e6c3ca1845eaf6537ef0f0
rizypb <rizwanul.rudra@ypbsystems.com>
2025-08-07 17:19:11 +0700

    fix: remove gs1 stuff

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,2 @@
 import QRCodeVue3 from "./QRCodeVue3.vue";
-import GS1QRCodeVue3 from "./GS1QRCodeVue3.vue";
-export { QRCodeVue3, GS1QRCodeVue3 };
+export default QRCodeVue3;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 import { default as QRCodeVue3 } from "./QRCodeVue3.vue";
-<<<<<<< HEAD
-import { default as GS1QRCodeVue3 } from "./GS1QRCodeVue3.vue";
-=======
->>>>>>> parent of 49366d6 (feat: gs1 standard QR codes)
-
 export default QRCodeVue3;
